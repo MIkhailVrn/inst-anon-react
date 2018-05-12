9f4e4b1a2d6c0769d1c58e67a38f67ff6ac5b95a
Macbook Pro <macbookpro@MacBook-Pro-Oleg.local>
2018-05-13 01:06:17 +0300

    sd

--- src/index.js
+++ src/index.js
@@ -52,6 +52,14 @@ export default class Search extends React.Component {
     return (
       <div>
         <div id="Search" className="container text-center appWrapper">
+
+          <a className="tgCont" href="t.me/inst_anon_bot" >
+            <div className="tgInfo">
+              <div className="tgLabel">Наш телеграм бот</div>    
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png" width="40" height="40"/>
+            </div>
+          </a>  
+
           <div className="row text-center">
             <SearchControl onSearch={() => this.handleSearch()} classSearch={this.state.searchWrapperClass} onChange={this.handleChange.bind(this)} searchValue={this.state.searchValue}/>
           </div>
