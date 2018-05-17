5825007c69856c51d2881050d6154c9a192f2e4d
Macbook Pro <macbookpro@MacBook-Pro-Oleg.local>
2018-05-18 01:07:33 +0300

    sd

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./css/bootstrap.min.css";
 import $ from 'jquery';
-import {Carousel} from 'react-bootstrap';
-import {FormControl} from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './css/index.css';
 import Spinner from 'react-spinkit';
+//import SlideMenu from 'react-slide-menu'
+import { bubble as Menu } from 'react-burger-menu'
+
 
 export default class Search extends React.Component {
   constructor() {
@@ -24,12 +27,12 @@ export default class Search extends React.Component {
     }
   }
 
-  componentDidMount() { 
+  componentDidMount() {
     document.title = "Inst Anon";
   }
 
   handleChange(event) {
-    this.setState({searchValue: event.target.value});
+    this.setState({ searchValue: event.target.value });
   }
 
   handleSearch() {
@@ -55,13 +58,13 @@ export default class Search extends React.Component {
 
           <a className="tgCont" href="https://t.me/inst_anon_bot" >
             <div className="tgInfo">
-              <div className="tgLabel">Наш телеграм бот</div>    
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png" width="40" height="40"/>
+              <div className="tgLabel">Наш телеграм бот</div>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png" width="40" height="40" />
             </div>
-          </a>  
+          </a>
 
           <div className="row text-center">
-            <SearchControl onSearch={() => this.handleSearch()} classSearch={this.state.searchWrapperClass} onChange={this.handleChange.bind(this)} searchValue={this.state.searchValue}/>
+            <SearchControl onSearch={() => this.handleSearch()} classSearch={this.state.searchWrapperClass} onChange={this.handleChange.bind(this)} searchValue={this.state.searchValue} />
           </div>
           <div className="row text-center">
             <div className="col-md-6 col-md-offset-3 mediaCarousel">
@@ -69,7 +72,7 @@ export default class Search extends React.Component {
                 <Spinner name="ball-spin-fade-loader" />
               </div>
               <MediaCarousel media={this.state.carouselMedia} isVisible={this.state.carouselVisible} isNoDataVisible={this.state.noDataVisible}
-                             isNoUserVisible={this.state.noUserVisible} isPrivate={this.state.isPrivate}/>
+                isNoUserVisible={this.state.noUserVisible} isPrivate={this.state.isPrivate} />
             </div>
           </div>
         </div>
@@ -92,9 +95,9 @@ class SearchControl extends React.Component {
     return (
       <div className={this.props.classSearch}>
         <div className="col-md-4 col-md-offset-4">
-          <FormControl className="searchInput" value={searchValue} onChange={this.onChange.bind(this)} onEnter={() => this.props.onSearch()}/>
+          <FormControl className="searchInput" value={searchValue} onChange={this.onChange.bind(this)} onEnter={() => this.props.onSearch()} />
           <Button bsStyle="success" className="searchBtn" onClick={() => this.props.onSearch()}>
-              Поиск
+            Поиск
           </Button>
         </div>
       </div>
@@ -105,25 +108,25 @@ class SearchControl extends React.Component {
 class MediaCarousel extends React.Component {
   render() {
 
-    if (this.props.media.length > 0){
+    if (this.props.media.length > 0) {
       var className = 'mediaCarousel ' + ((this.props.isVisible && this.props.media.length > 0) ? 'active' : 'inactive');
       return (
         <div className={className}>
           <Carousel interval={null}>
             {
-              this.props.media.map(function(item) {
+              this.props.media.map(function (item) {
 
                 var mediaContainer;
                 if (item.type === 'image') {
-                  mediaContainer = <img className="imageCarousel" src={item.url[0].url}/>;
+                  mediaContainer = <img className="imageCarousel" src={item.url[0].url} />;
                 } else {
-                  mediaContainer = <video className="videoCarousel" controls><source src={item.url} type="video/mp4"/></video>;
+                  mediaContainer = <video className="videoCarousel" controls><source src={item.url} type="video/mp4" /></video>;
                 }
-                  return <Carousel.Item className="carouselItem">
-                    {mediaContainer}
-                    <Carousel.Caption>
-                      <h3></h3>
-                      <p></p>
+                return <Carousel.Item className="carouselItem">
+                  {mediaContainer}
+                  <Carousel.Caption>
+                    <h3></h3>
+                    <p></p>
                   </Carousel.Caption>
                 </Carousel.Item>
               })
@@ -133,12 +136,12 @@ class MediaCarousel extends React.Component {
     } else {
       var className = this.props.isNoDataVisible || this.props.isNoUserVisible || this.props.isPrivate ? 'active' : 'inactive';
       var text;
-      if (this.props.isNoDataVisible){
-          text = "У Пользователя нет активных историй(";
+      if (this.props.isNoDataVisible) {
+        text = "У Пользователя нет активных историй(";
       } else if (this.props.isNoUserVisible) {
-          text = "Такого пользователя не существует";
+        text = "Такого пользователя не существует";
       } else if (this.props.isPrivate) {
-          text = "Данный пользователь скрыл свой аккаунт. В настоящий момент возможность просмотра приватных аккаунтов находится в разработке...";
+        text = "Данный пользователь скрыл свой аккаунт. В настоящий момент возможность просмотра приватных аккаунтов находится в разработке...";
       };
       return (
         <div className={className}>
@@ -148,46 +151,57 @@ class MediaCarousel extends React.Component {
   }
 }
 
-ReactDOM.render(
- <Search/ >, document.getElementById('root'));
+/* ReactDOM.render(
+  <div id="outer-container">
+    <Menu >
+      <a id="home" className="menu-item" href="/">Главная</a>
+      <a id="about" className="menu-item" href="/about">О Сервисе</a>
 
-   /* ------additional functions----- */
+    </Menu>
+    <Search />
+  </div>,
+  document.getElementById('root')); */
+
+  ReactDOM.render(
+      <Search />,
+    document.getElementById('root'));
+
+/* ------additional functions----- */
 
 
 
 
-   function getStoriesArrayForUser (oControl)
-   { 
-     //var sUrl = "https://inst-anon.herokuapp.com/userStories?userName=" + oControl.state.searchValue.toLowerCase();
-     //var sUrl = "https://inst-anon3-mikhail36.c9users.io/userStories?userName=" + oControl.state.searchValue;
-     var sUrl = "https://inst-anon3-mikhail36.c9users.io/userStories?userName=" + oControl.state.searchValue.toLowerCase();
+function getStoriesArrayForUser(oControl) {
+  //var sUrl = "https://inst-anon.herokuapp.com/userStories?userName=" + oControl.state.searchValue.toLowerCase();
+  //var sUrl = "https://inst-anon3-mikhail36.c9users.io/userStories?userName=" + oControl.state.searchValue;
+  var sUrl = "https://inst-anon3-mikhail36.c9users.io/userStories?userName=" + oControl.state.searchValue.toLowerCase().replace(/\s/g, '');
 
 
-     $.ajax({
-      url: sUrl,
-      dataType: 'json',
-      crossDoamin: true,
-      cache: false,
-      success: function(data, status, xhr) {
+  $.ajax({
+    url: sUrl,
+    dataType: 'json',
+    crossDoamin: true,
+    cache: false,
+    success: function (data, status, xhr) {
 
-        oControl.setState({carouselMedia: data});
-        if (data.length === 0){
-          if (xhr.status === 202){ // is private
-            oControl.setState({noDataVisible: false});
-            oControl.setState({isPrivate: true});
-          } else {
-            oControl.setState({noDataVisible: true});
-            oControl.setState({isPrivate: false});
-          }
+      oControl.setState({ carouselMedia: data });
+      if (data.length === 0) {
+        if (xhr.status === 202) { // is private
+          oControl.setState({ noDataVisible: false });
+          oControl.setState({ isPrivate: true });
         } else {
-          oControl.setState({noDataVisible: false});
+          oControl.setState({ noDataVisible: true });
+          oControl.setState({ isPrivate: false });
         }
-        oControl.setState({noUserVisible: false});
-        oControl.setState({isBusyVisible: false});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        oControl.setState({noUserVisible: true});
-        oControl.setState({isBusyVisible: false});
-      }.bind(this)
-    });
+      } else {
+        oControl.setState({ noDataVisible: false });
+      }
+      oControl.setState({ noUserVisible: false });
+      oControl.setState({ isBusyVisible: false });
+    }.bind(this),
+    error: function (xhr, status, err) {
+      oControl.setState({ noUserVisible: true });
+      oControl.setState({ isBusyVisible: false });
+    }.bind(this)
+  });
 }
